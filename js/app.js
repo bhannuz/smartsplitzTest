d54c22bec18aa901454b9388c43ee834db14f2ab
bhannuz <bhanu_85@aol.com>
2026-04-29 16:32:30 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getDatabase, ref, get, push, set, update, remove }
   from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
